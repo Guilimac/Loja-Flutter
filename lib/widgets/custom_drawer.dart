b3c38b61b1ed4d59d0e0227a3cdbab9f3bd62d66
Guilherme de Lima Conceição <guilherme.lima@ed.company>
2021-01-06 21:19:54 -0300

    drawer tiles itens do menu

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/tiles/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     Widget _buildDrawerBack() => Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Color.fromARGB(255, 203,236,241),
-            Colors.white
-          ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
-    );
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color.fromARGB(255, 203, 236, 241), Colors.white],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter)),
+        );
     return Drawer(
       child: Stack(
         children: [
           _buildDrawerBack(),
           ListView(
-            padding: EdgeInsets.only(left: 32,top: 16),
+            padding: EdgeInsets.only(left: 32, top: 16),
             children: [
               Container(
                 margin: EdgeInsets.only(bottom: 8),
@@ -26,16 +26,40 @@ class CustomDrawer extends StatelessWidget {
                   children: [
                     Positioned(
                         top: 8,
-                        left:0,
-                        child: Text("Flutter's \nClothing",
-                          style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                        ))
+                        left: 0,
+                        child: Text(
+                          "Flutter's \nClothing",
+                          style: TextStyle(
+                              fontSize: 32, fontWeight: FontWeight.bold),
+                        )),
+                    Positioned(left: 0, bottom: 0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Olá,",
+                        style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),
+                        ),
+                        GestureDetector(
+                          child: Text("Entre ou Cadastre-se >",
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold
+                            ),),
+                          onTap: (){},
+                        )
+                      ],
+                    ),)
                   ],
                 ),
-              )
+              ),
+              Divider(),
+              DrawerTile(Icons.home, "Inicio"),
+              DrawerTile(Icons.list, "Produtos"),
+              DrawerTile(Icons.location_on, "Lojas"),
+              DrawerTile(Icons.playlist_add_check, "Meus Pedidos"),
             ],
           )
-
         ],
       ),
     );
