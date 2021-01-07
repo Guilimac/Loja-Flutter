114440e3b730ad0adff8b933ee8a8d218a244892
Guilherme de Lima Conceição <guilherme.lima@ed.company>
2021-01-06 21:28:13 -0300

    estrutura para mudança de cor dos tiles

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:loja_virtual/tiles/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
+  final PageController pageController;
+  CustomDrawer(this.pageController);
   @override
   Widget build(BuildContext context) {
     Widget _buildDrawerBack() => Container(
@@ -54,10 +56,10 @@ class CustomDrawer extends StatelessWidget {
                 ),
               ),
               Divider(),
-              DrawerTile(Icons.home, "Inicio"),
-              DrawerTile(Icons.list, "Produtos"),
-              DrawerTile(Icons.location_on, "Lojas"),
-              DrawerTile(Icons.playlist_add_check, "Meus Pedidos"),
+              DrawerTile(Icons.home, "Inicio",pageController,0),
+              DrawerTile(Icons.list, "Produtos",pageController,1),
+              DrawerTile(Icons.location_on, "Lojas",pageController,2),
+              DrawerTile(Icons.playlist_add_check, "Meus Pedidos",pageController,3),
             ],
           )
         ],
