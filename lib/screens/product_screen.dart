6bb4e1c8677737f0020f987ce52b2c133c3b257e
Guilherme de Lima Conceição <guilherme.lima@ed.company>
2021-01-26 18:58:39 -0300

    seleção de tamanho de produto

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
 
@@ -12,6 +13,7 @@ class ProductScreen extends StatefulWidget {
 
 class _ProductScreenState extends State<ProductScreen> {
   final ProductData product;
+  String size;
   _ProductScreenState(this.product);
   @override
   Widget build(BuildContext context) {
@@ -54,6 +56,51 @@ class _ProductScreenState extends State<ProductScreen> {
                       fontSize: 22,
                       fontWeight: FontWeight.bold,
                       color: primaryColor
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                      "Tamanho",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500
+                    ),
+                  ),
+                  SizedBox(
+                    height: 34,
+                    child: GridView(
+                      scrollDirection: Axis.horizontal,
+                      padding: EdgeInsets.symmetric(vertical: 4),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        mainAxisSpacing: 8,
+                        childAspectRatio: 0.5,
+                      ),
+                      children: product.sizes.map((s){
+                        return GestureDetector(
+                          onTap: (){
+                            setState(() {
+                              size = s;
+                            });
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(4)),
+                              border: Border.all(
+                                color: s == size ? primaryColor : Colors.grey[500],
+                                width: 3
+                              ),
+                            ),
+                            width: 50,
+                            alignment: Alignment.center,
+                            child: Text(
+                              s
+                            ),
+                          ),
+                        );
+                      }).toList(),
                     ),
                   )
                 ],
